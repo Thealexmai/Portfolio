3b9687fdb25883fa23b39c8a0a4ef73ccd88520b
Alex <amai2@u.rochester.edu>
2016-12-13 18:39:55 -0500

    Add form validation

--- contact.php
+++ contact.php
@@ -20,26 +20,26 @@
 
 		    <h1>Contact Me!</h1>
 
-			<form method="post" action="database-write.php" id="contactForm">
+			<form method="post" action="database-write.php" id="contactForm" name="contactForm" onsubmit="return(validate());">
 
 				<div>
 					<label for="firstname">First Name:</label>
-					<input type="text" name="firstname" id="firstname" required>
+					<input type="text" name="firstname" id="firstname">
 				</div>
 
 				<div>
 					<label for="lastname">Last Name:</label>
-					<input type="text" name="lastname" id="lastname" required>
+					<input type="text" name="lastname" id="lastname">
 				</div>
 
 				<div>
 					<label for="email">Email:</label>
-					<input type="email" name="email" id="email" required>
+					<input type="email" name="email" id="email">
 				</div>
 
 				<div>
 					<label for="telephone">Telephone:</label>
-					<input type="tel" name="phone" id="phone" required>
+					<input type="tel" name="phone" id="phone">
 				</div>
 
 				<div>
@@ -56,6 +56,42 @@
 
 	<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
 	<script src="js/menu-highlighter.js"></script>
+	<!-- Form validation -->
+	<script type="text/javascript">
+		function validate() {
+			if(document.contactForm.firstname.value == "") {
+				alert("Please provide your first name!");
+				document.contactForm.firstname.focus();
+				return false;
+			}
+			if(document.contactForm.lastname.value == "") {
+				alert("Please provide your last name!");
+				document.contactForm.lastname.focus();
+				return false;
+			}
+			var emailID = document.contactForm.email.value;
+	        atpos = emailID.indexOf("@");
+	        dotpos = emailID.lastIndexOf(".");
+	         
+	        if (atpos < 1 || ( dotpos - atpos < 2 )) {
+	        	alert("Please enter a correct email address");
+	            document.contactForm.email.focus();
+	            return false;
+	        }
+	        if(document.contactForm.phone.value == "") {
+				alert("Please provide your phone number!");
+				document.contactForm.phone.focus();
+				return false;
+			}
+			if(document.contactForm.message.value == "") {
+				alert("Did you want to write a message to me?");
+				document.contactForm.message.focus();
+				return false;
+			}
+
+			return true;
+		}
+	</script>
 </body>
 
 </html>
